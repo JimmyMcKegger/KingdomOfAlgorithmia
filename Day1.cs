966b92198665e47a3569281a1893a2f50e6297cd
James <jamesrobertsongroves@gmail.com>
2024-11-13 00:51:17 +0000

    day 1

--- Day1.cs
+++ Day1.cs
@@ -1,10 +1,16 @@
-namespace KingdomOfAlgorithmia
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace KingdomOfAlgorithmia.Day1
 {
-    public class Day1()
+    public static class Day1
     {
         private static string p1 = "d1p1.txt";
         private static string p2 = "d1p2.txt";
         private static string p3 = "d1p3.txt";
+        
         public static void Solve()
         {
             Console.WriteLine($"Part 1: {Part1(p1)}");
@@ -23,18 +29,20 @@ namespace KingdomOfAlgorithmia
         
         private static int Part2(string input)
         {
-            return new StreamReader(input)
-                .ReadToEnd()
-                .TrimEnd()
-                .Chunk(2)
-                .Aggregate(0, (acc, chunk) => acc + PotionCount(chunk));
+            return Battle(input, 2);
         }
+
         private static int Part3(string input)
+        {
+            return Battle(input, 3);
+        }
+
+        private static int Battle(string input, int steps)
         {
             return new StreamReader(input)
                 .ReadToEnd()
                 .TrimEnd()
-                .Chunk(3)
+                .Chunk(steps)
                 .Aggregate(0, (acc, chunk) => acc + PotionCount(chunk));
         }
         
@@ -56,16 +64,16 @@ namespace KingdomOfAlgorithmia
             {
                 result += 2;
             }
-            else if( enemyCount == 3)
+            else if (enemyCount == 3)
             {
                 result += 6;
             }
             return result;
         }
         
-        private static Dictionary<char, int> legend = new Dictionary<char, int>()
+        private static readonly Dictionary<char, int> legend = new Dictionary<char, int>
         {
-            { 'A', 0},
+            { 'A', 0 },
             { 'B', 1 },
             { 'C', 3 },
             { 'D', 5 }
